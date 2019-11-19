20c7d4169af277ab91e00153889815deb9e91a3c
Gareth <gvs.0027@gmail.com>
2019-11-19 15:14:47 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="main-app" class="container">
+    <h4>{{title}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainApp",
+  data: function() {
+    return {
+      title: "Appointment List"
+    }
+  }
+};
+</script>
