313df953ed37b7b32c76a2b7f50a71031fe4240e
Gareth <gvs.0027@gmail.com>
2019-11-19 18:59:22 +0100

    Add delete entries event handler

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="main-app" class="container">
     <div class="row"></div>
-    <appointment-list :appointments="appointments" />
+    <appointment-list :appointments="appointments" @remove="removeItem" />
 
   </div>
 </template>
 
 <script>
 import AppointmentList from "./components/AppointmentList";
+import _ from "lodash";
 import axios from "axios";
 
 export default {
@@ -26,6 +27,11 @@ export default {
     axios
       .get("./data/appointments.json")
       .then(response => (this.appointments = response.data));
+  },
+  methods: {
+    removeItem: function(apt) {
+      this.appointments = _.without(this.appointments, apt);
+    }
   }
 };
 </script>
